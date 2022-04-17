3fb3bdfc6b5c2b158b3448a3a3a31757ca1e7044
knightlyf <varunhydra01@gmail.com>
2022-04-17 16:23:15 +0530

    initial commit

---
+++ src/com/ibm/application/Application.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.ibm.application;
+
+/**
+ * @author 003BD2744
+ *
+ */
+public class Application {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Eclipse Proj");
+	}
+
+}
